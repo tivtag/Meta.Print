cf6389b2ace06867f9bfca249691aabd2e02fb81
Paul Ennemoser <tick@federrot.at>
2012-09-20 19:19:31 +0200

    Initial commit
    .. and last commit?

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "Meta.Print" )]
+[assembly: AssemblyDescription("A [silly] self-printing C# program - a quine.")]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "federrot Software" )]
+[assembly: AssemblyProduct( "Meta.Print" )]
+[assembly: AssemblyCopyright( "Copyright © federrot Software 2012" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+[assembly: Guid( "fc061f41-1c94-435d-b6ad-7fbadbd002c2" )]
+
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
